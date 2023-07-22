f9e47d09f0382620b019cf0747a1a747675980f7
SamoProgrammer <samoprogrammer@gmail.com>
2023-07-22 11:12:00 +0330

    init long text input

---
+++ lib/widgets/long_text_form_field_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LongTextFormFieldWidget extends StatefulWidget {
+  final String lable;
+  const LongTextFormFieldWidget({super.key, required this.lable});
+
+  @override
+  State<LongTextFormFieldWidget> createState() =>
+      _LongTextFormFieldWidgetState();
+}
+
+class _LongTextFormFieldWidgetState extends State<LongTextFormFieldWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.centerRight,
+          child: Text(
+            widget.lable,
+            style: const TextStyle(fontSize: 18),
+          ),
+        ),
+        TextFormField(
+          maxLines: null,
+          decoration: const InputDecoration(
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 0.5))),
+        ),
+      ],
+    );
+  }
+}
