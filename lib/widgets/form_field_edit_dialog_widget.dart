e9323b960d5866a6ebdba5a8e249e8bb1cae6a88
SamoProgrammer <samoprogrammer@gmail.com>
2023-07-31 10:16:40 +0330

    fix namings & and condition for options

--- lib/widgets/form_field_edit_dialog_widget.dart
+++ lib/widgets/form_field_edit_dialog_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:form_generator/database/models/form_element.dart';
+import 'package:form_generator/database/models/form_element_type.dart';
 import 'package:form_generator/widgets/info_button_widget.dart';
 import 'package:form_generator/widgets/warning_button_widget.dart';
 
@@ -13,9 +14,19 @@ class FormFieldEditDialogWidget extends StatefulWidget {
 }
 
 class _FormFieldEditDialogWidgetState extends State<FormFieldEditDialogWidget> {
-  var formFieldNameController = TextEditingController();
-  var formFieldLableController = TextEditingController();
-  var formFieldOptionsController = TextEditingController();
+  var txtNameController = TextEditingController();
+  var txtLabelController = TextEditingController();
+  var txtOptionsController = TextEditingController();
+  bool isRadioButton = false;
+
+  @override
+  void initState() {
+    if (widget.element.type == FormElementType.radioButton) {
+      isRadioButton = true;
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -23,43 +34,58 @@ class _FormFieldEditDialogWidgetState extends State<FormFieldEditDialogWidget> {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisSize: MainAxisSize.min,
         children: [
-          const Text("نام فیلد (انگلیسی)"),
-          SizedBox(
-            width: MediaQuery.of(context).size.width / 6,
-            height: 40,
-            child: TextFormField(
-              controller: formFieldNameController,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black, width: 0.5))),
-            ),
-          ),
+          // const Text("نام فیلد (انگلیسی)"),
+          // SizedBox(
+          //   width: MediaQuery.of(context).size.width / 6,
+          //   height: 40,
+          //   child: TextFormField(
+          //     controller: txtNameController,
+          //     decoration: const InputDecoration(
+          //         border: OutlineInputBorder(
+          //             borderSide: BorderSide(color: Colors.black, width: 0.5))),
+          //   ),
+          // ),
           const Text("عنوان فیلد"),
           SizedBox(
             width: MediaQuery.of(context).size.width / 6,
             height: 40,
             child: TextFormField(
-              controller: formFieldNameController,
+              controller: txtNameController,
               decoration: const InputDecoration(
                   border: OutlineInputBorder(
                       borderSide: BorderSide(color: Colors.black, width: 0.5))),
             ),
           ),
-          const Text("گزینه ها (با ویرگول جدا کنید)"),
-          SizedBox(
-            width: MediaQuery.of(context).size.width / 6,
-            height: 40,
-            child: TextFormField(
-              controller: formFieldNameController,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black, width: 0.5))),
-            ),
-          )
+          if (isRadioButton)
+            Column(
+              children: [
+                const Text("گزینه ها (با ویرگول جدا کنید)"),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 6,
+                  height: 40,
+                  child: TextFormField(
+                    controller: txtOptionsController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 0.5))),
+                  ),
+                )
+              ],
+            )
         ],
       ),
       actions: [
-        InfoButtonWidget(text: "ذخیره", onPressed: () {}),
+        InfoButtonWidget(
+            text: "ذخیره",
+            onPressed: () {
+              setState(() {
+                widget.element.label = txtLabelController.text;
+                var newOptions = txtOptionsController.text.split(",");
+                widget.element.options = newOptions;
+              });
+              Navigator.pop(context);
+            }),
         WarningButtonWidget(
             text: "خروج",
             onPressed: () {
