b6d304325b22a841ff540fc1a153c35673aa7fd6
SamoProgrammer <samoprogrammer@gmail.com>
2023-08-12 11:47:39 +0330

    init enum

--- lib/models/form_element_type.dart
+++ lib/models/form_element_type.dart
@@ -1,16 +1,7 @@
-import 'package:hive_flutter/hive_flutter.dart';
-part 'form_element_type.g.dart';
-
-@HiveType(typeId: 2)
 enum FormElementType {
-  @HiveField(0)
   description,
-  @HiveField(1)
   shortText,
-  @HiveField(2)
   multiLineText,
-  @HiveField(3)
   radioButton,
-  @HiveField(4)
   checkBox
 }
