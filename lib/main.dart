8881d4a8545e35b5e9eb7919a713fdf81111ff4a
SamoProgrammer <samoprogrammer@gmail.com>
2023-08-12 11:45:21 +0330

    init routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,51 @@
+import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:form_generator/models/form_element.dart';
-import 'package:form_generator/models/form_element_type.dart';
-import 'package:form_generator/pages/edit_form_page.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:form_generator/api/form_element_api_service.dart';
+import 'package:form_generator/models/form_element_model.dart';
+import 'package:form_generator/pages/answer_form_page.dart';
+import 'package:form_generator/pages/recent_forms_page.dart';
 
 void main() async {
-  await Hive.initFlutter();
-  Hive.registerAdapter(FormElementTypeAdapter());
-  Hive.registerAdapter(FormElementAdapter());
-  runApp(const FormGeneratorApp());
+  runApp(
+    const FormGeneratorApp(),
+  );
 }
 
 class FormGeneratorApp extends StatelessWidget {
   const FormGeneratorApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final FormElementModelApiService formElementModelApiService =
+        FormElementModelApiService();
+
+    final routerDelegate = BeamerDelegate(
+      locationBuilder: RoutesLocationBuilder(
+        routes: {
+          // Return either Widgets or BeamPages if more customization is needed
+          '/': (context, state, data) => const RecentFormsPage(),
+          // '/forms': (context, state, data) => BooksScreen(),
+          '/forms/:formId': (context, state, data) {
+            // Take the path parameter of interest from BeamState
+            final formId = state.pathParameters['formId']!;
+            final formElements = <FormElementModel>[];
+            formElementModelApiService
+                .getFormElementModelsById(int.parse(formId))
+                .then((value) => value);
+            return BeamPage(
+              key: ValueKey(formId),
+              title: 'فرم آنلاین',
+              popToNamed: '/',
+              type: BeamPageType.scaleTransition,
+              child: AnswerFormPage(formElements: formElements),
+            );
+          }
+        },
+      ),
+    );
+
     return MaterialApp(
-      home: const EditFormPage(),
+      home: const RecentFormsPage(),
       localizationsDelegates: const [
         GlobalCupertinoLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
