77c2f94a5486a306b36cc21b1588ac4c1ae18e75
SamoProgrammer <samoprogrammer@gmail.com>
2023-08-13 22:41:26 +0330

    init answer form page

--- lib/pages/answer_form_page.dart
+++ lib/pages/answer_form_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:form_generator/models/form_element_model.dart';
+import 'package:form_generator/widgets/form_element_widget.dart';
 
 class AnswerFormPage extends StatefulWidget {
   final List<FormElementModel> formElements;
-  const AnswerFormPage({super.key,required this.formElements});
+  const AnswerFormPage({super.key, required this.formElements});
 
   @override
   State<AnswerFormPage> createState() => _AnswerFormPageState();
@@ -12,6 +13,17 @@ class AnswerFormPage extends StatefulWidget {
 class _AnswerFormPageState extends State<AnswerFormPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.75,
+            height: MediaQuery.of(context).size.height,
+            child: ListView.builder(
+                itemCount: widget.formElements.length,
+                itemBuilder: (context, index) {
+                  return FormElementWidget(element: widget.formElements[index]);
+                })),
+      ),
+    );
   }
 }
