1022597ad9e2680622eda2b49e5bf4bdf1889520
SamoProgrammer <samoprogrammer@gmail.com>
2023-08-21 10:07:02 +0330

    fix user api service and boost performance

--- lib/api/user_api_service.dart
+++ lib/api/user_api_service.dart
@@ -30,48 +30,39 @@ class UserApiService {
   }
 
   Future<bool> loginUser(String username, String password) async {
-    try {
-      final response = await _dio.get(
-        '$baseUrl/Login',
-        queryParameters: {
-          'username': username,
-          'password': password,
-        },
-      );
-      if (response.statusCode == 200) {
-        // Login successful
-        // You can access the token using response.data['token']
-        saveToken(response.data['token'], username, password);
-        return true;
-      } else {
-        return false;
-        // Handle error
-      }
-    } catch (error) {
+    final response = await _dio.post(
+      '$baseUrl/Login',
+      queryParameters: {
+        'username': username,
+        'password': password,
+      },
+    );
+    if (response.statusCode == 200) {
+      // Login successful
+      // You can access the token using response.data['token']
+      saveToken(response.data['token'], username, password);
+      return true;
+    } else {
+      return false;
       // Handle error
     }
-    return false;
   }
 
   Future<String> testLogin(String token) async {
-    try {
-      final options = Options();
-      options.headers!['Authorization'] = 'Bearer $token';
-      final response = await _dio.get('$baseUrl/TestLogin', options: options);
-      if (response.statusCode == 200) {
-        // Login successful
-        return response.data;
-      } else {
-        return 'false';
-        // Handle error
-      }
-    } catch (error) {
+    var headers = {'Authorization': 'Bearer $token'};
+    final response = await _dio.get('$baseUrl/TestLogin',
+        options: Options(headers: headers));
+    if (response.statusCode == 200) {
+      // Login successful
+      print(response.data);
+      return response.data;
+    } else {
+      return 'false';
       // Handle error
     }
-    return 'false';
   }
 
-  Future<void> saveToken(String token, String username, String password) async {
+  void saveToken(String token, String username, String password) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString("token", token);
     prefs.setString("username", username);
